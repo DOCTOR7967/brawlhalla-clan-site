950d8ee51e9b51e37a356146309418338a5aa573
DOCTOR7967 <88008312+DOCTOR7967@users.noreply.github.com>
2026-06-26 14:50:18 -0300

    upuuuuuu

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
-bla = process.cwd();
 const express = require("express");
 const path = require("path");
-const { getRankedPlayers } = require("./api/update-elos");
+const { getTop12Guild } = require("./api/update-elos");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-const DEFAULT_IDS = ["81437113"];
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public" , "/index.html"));
-})
-app.set("json spaces", 2);
-app.use(express.static("public"));
-app.use(express.json());
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
+// 🔥 RANKING 12 MELHORES
 app.get("/api/update-elos", async (req, res) => {
   try {
-    console.log("ROTA CHAMADA");
-
-    const players = await getRankedPlayers();
-
-    console.log("PLAYERS:", players);
-
-    res.json(players);
-
+    const data = await getTop12Guild("2616831");
+    res.json(data);
   } catch (err) {
-    console.error("ERRO REAL:", err); // 🔥 isso aqui é o mais importante
-
-    res.status(500).json({
-      error: err.message
-    });
+    res.status(500).json({ error: err.message });
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
